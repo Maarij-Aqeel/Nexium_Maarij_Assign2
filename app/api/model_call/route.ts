43d7a03f4a4ca785db7a8a077f6dc7a3ef2bbc8d
Maarij-Aqeel <maarijaqeel3200@gmail.com>
2025-07-10 22:53:57 +0500

    Fixed: issues with api

--- app/api/model_call/route.ts
+++ app/api/model_call/route.ts
@@ -5,6 +5,11 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const Api_key=process.env.API_KEY
+
+if (!Api_key)
+{
+  throw new Error("Missing Api_Key in environment variables")
+}
 const genai=new GoogleGenerativeAI(Api_key)
 const systemPrompt="You are a professional content summarizer. Given a raw blog text that may include inconsistencies, formatting issues, or irrelevant data (like ads or links), extract and summarize the main points accurately and concisely. Focus on preserving all essential information, key insights, and the original tone. Ignore garbage data. Return a clean and readable summary. Your Response should always start like 'This comprehensive blog post explores....' or something similar to it. There can be bullet-points or headings etc."
 
